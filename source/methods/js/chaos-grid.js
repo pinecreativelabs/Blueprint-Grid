0c194d664ede54474d9e952274ab6ca313e69c1a
Pine Creative Labs <brad.fogelstrom@gmail.com>
2025-06-18 09:14:41 -0400

    2.2

--- source/methods/js/chaos-grid.js
+++ source/methods/js/chaos-grid.js
@@ -3,8 +3,10 @@ window.addEventListener("load", function randomize(){
 	var m;
 	var tr;
 	var kx;
+	var sc;
     var listm = document.querySelectorAll('.chaos-grid.randomize > *');
 	var listtrx = document.querySelectorAll('.chaos-grid.randomize.tilt > *');
+	var listsc = document.querySelectorAll('.chaos-grid.randomize.scale > *');
     for(var i=0;i<listm.length;i++){
 		m = Math.floor(Math.random()*280-140);
         listm[i].style.margin = m + "px";
@@ -13,5 +15,10 @@ window.addEventListener("load", function randomize(){
 		tr = Math.floor(Math.random()*70-35);
 		kx = Math.floor(Math.random()*45-45);
 		listtrx[i].style.transform = "rotate(" + tr + "deg) skew(" + kx + "deg)";
+    }
+	for(var i=0;i<listsc.length;i++){
+		var precision = 100;
+		sc = Math.floor(Math.random() * (3 * precision - 1 * precision) + 0.5 * precision) / (1*precision);
+        listsc[i].style.scale = sc;
     }
 });
